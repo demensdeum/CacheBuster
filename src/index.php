d3a51183967afdd4c68a09133f1da3b634f97ccf
Ilia Prokhorov <demensdeum@gmail.com>
2024-05-11 14:47:29 +0400

    iframe instead of redirect

--- src/index.php
+++ src/index.php
@@ -9,7 +9,34 @@ if (count($dirs) > 0) {
     $dirname = basename($largest_dir);
     $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . $_SERVER['REQUEST_URI'] . '/' . $dirname;
     $current_url = str_replace("index.php", "", $current_url);
-    header("Location: $current_url");
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Redirect</title>
+        <style>
+            body, html {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+            }
+            iframe {
+                border: none;
+                width: 100%;
+                height: 100%;
+                display: block;
+            }
+        </style>
+    </head>
+    <body>
+        <iframe src="<?php echo $current_url; ?>" frameborder="0"></iframe>
+    </body>
+    </html>
+
+    <?php
     exit;
 } else {
     echo "No numeric folder in script directory!";
